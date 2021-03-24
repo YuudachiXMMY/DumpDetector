7e20b983a368a5508ed44c68128a27c9eeb2d4e6
YuudachiXMMY <934647325@qq.com>
2021-03-24 15:47:41 +0800

    delete test files

--- t.py
+++
@@ -1,7 +0,0 @@
-import requests as rq
-
-kw = {'wd':'长城'}
-
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
-
-response = rq.get("172.29.157.40", params = kw, headers = headers)
